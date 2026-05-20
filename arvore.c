bef03614e3119829bd278845748d24232e9768a2
prof Barros <anderson.barros@ifpi.edu.br>
2026-05-20 19:45:32 -0300

    Terceniro commit

--- arvore.c
+++ arvore.c
@@ -1,6 +1,24 @@
+void menuInsercao() {
+    int op;
+    do {
+        
+        printf("\n--- Metodo de Insercao ---\n");
+        printf("1. Inserir no Inicio\n");
+        printf("2. Inserir no Fim\n");
+        printf("3. Inserir no Meio\n");
+        printf("4. Voltar\n");
+        printf("Opcao: ");
+        scanf("%d", &op);
+
+        
+
+    } while (op != 4);
+}
 int main(){
 
     printf("Menu da aplicação Árvore");
-    
+    menuInsercao();
+
+
     return 0;
 }
